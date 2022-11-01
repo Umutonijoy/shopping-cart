f46739999a3f9e74ca19527cb6ef8af2d7bb3c5f
Umutonijoy <umutonijoie303@gmail.com>
2022-11-01 09:03:57 +0200

    new

---
+++ src/data.js
@@ -0,0 +1,29 @@
+import image1 from '../src/component/images/f5.jpeg'
+import image2 from '../src/component/images/f2.jpeg'
+import image3 from '../src/component/images/f3.jpeg'
+
+
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Bananas',
+      price: 1400,
+      image: image1,
+    },
+    {
+      id: '2',
+      name: 'Lemons',
+      price: 2400,
+      image: image2,
+    },
+    {
+      id: '3',
+      name: 'Berrys',
+      price: 1000,
+      image: image3,
+    },
+    
+  ],
+};
+export default data;
